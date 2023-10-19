890aa2044f56697478592c67b2fbba8d9245b3b9
André Prado <andrepradooliveira16@gmail.com>
2023-10-18 23:44:42 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,28 +2,11 @@ import express from 'express'
 import {Router, Response, Request} from 'express'
 import alunoRouter from './routes/alunosRouter'
 
-
 const app = express()
-const route = Router()
+// const route = Router()
 const port = 3000
 
 app.use("/api", alunoRouter)
-
-// const listaAlunos = alunosServices.readAllAlunos();
-
-
-
-// const listaAlunos = {
-//     id:15,
-//     nome: "André"
-// }
-
-
-// route.get('/', (req:Request, res:Response)=>{
-//     res.json({data: listaAlunos})
-// })
-
-// app.use(route)
 
 app.listen(port, ()=>{console.log(`Server running on http://localhost:${port}`)})
 
