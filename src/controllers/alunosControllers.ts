e3b10d6c75a6fd1801f3edf4dc2f56c995b6dfbe
André Prado <andrepradooliveira16@gmail.com>
2023-10-19 10:33:54 -0300

    finishing alunoControlles

--- src/controllers/alunosControllers.ts
+++ src/controllers/alunosControllers.ts
@@ -5,15 +5,84 @@ class alunosControllers{
     constructor(){}
 
     async listarAlunos(req: Request, res: Response){
-        const listaAlunos = await alunosServices.readAllAlunos();
-        return res.status(200).json(
+        try{
+            const listaAlunos = await alunosServices.readAllAlunos();
+            return res.status(200).json(
             {
                 status:'ok',
                 alunos: listaAlunos
             }
         )
+        } catch(error){
+            res.status(500).json(error)
+        }
+        
     }
 
+    async buscaAluno(req: Request, res: Response){
+        try{
+            const matricula = req.params.matricula;
+            const alunoBuscado = await alunosServices.readAluno(matricula)
+            return res.status(200).json(
+                {
+                    status: 'ok',
+                    alunoBuscado,
+                }
+            )
+        } catch (error){
+            res.status(500).json(error)
+        }
+    }
+
+    async criarAluno(req: Request, res: Response){
+        try{
+            const {id, nome, curso, email, matricula} = req.body;
+            const criaAluno = await alunosServices.createAluno({id, nome, curso, email, matricula})
+            return res.status(200).json(
+                {
+                    status: 'ok',
+                    criaAluno,
+                }
+            )
+        } catch(error){
+            res.status(500).json(error)
+        }
+    }
+
+    async atualizarAluno(req: Request, res: Response){
+        try{
+            const id = req.params.id
+            const {nome, curso, email, matricula} = req.body;
+            const atualizaAluno = await alunosServices.updateAluno(id, {nome, curso, email, matricula})
+            return res.status(200).json(
+                {
+                    status: 'ok',
+                    atualizaAluno,
+                    message: 'Aluno atualizado com sucesso!!'
+                }
+            )
+
+        } catch(error){
+            res.status(500).json(error)
+        }
+    }
+
+    async deletaAluno(req: Request, res: Response){
+        try{
+            const id = req.params.id
+            const alunoDeletado = await alunosServices.deleteAlunos(id)
+            return res.status(200).json(
+                {
+                    status: 'ok',
+                    alunoDeletado,
+                    message: 'Aluno deletado com sucesso'
+                }
+            )
+
+        } catch(error){
+            res.status(500).json(error)
+        }
+    }
 }
 
 export default new alunosControllers()
